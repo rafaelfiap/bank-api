2d51ad2d14f18f572657bdf6d7b8cf34eff40fd2
rafaelfiap <rm557837@fiap.com.br>
2025-03-18 20:35:53 -0300

    Atualização Projeto Controller

--- src/main/java/br/com/fiap/bank/controller/ProjetoController.java
+++ src/main/java/br/com/fiap/bank/controller/ProjetoController.java
@@ -5,19 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Controller responsável pelo retorno das informações do projeto.
+ * Controlador responsável pelo retorno das informações do projeto.
+ *
+ * Este controlador fornece um endpoint que exibe o nome do projeto e os integrantes da equipe.
+ * A rota base foi configurada para que o endpoint responda corretamente quando acessado via 
+ * `http://localhost:8080/`.
+ *
+ * @author Rafael e Lucas
  * @since 1.0
- * @version 1.0
+ * @version 1.1
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/") // Define que a rota base termina com "/"
 public class ProjetoController {
 
     /**
      * Endpoint que retorna o nome do projeto e os integrantes da equipe.
-     * @return String com informações do projeto.
+     * 
+     * Acessível via `http://localhost:8080/`, esta rota exibe informações básicas sobre o sistema.
+     *
+     * @return String contendo o nome do projeto e os integrantes da equipe.
+     * @since 1.0
      */
-    @GetMapping
+    @GetMapping("/")
     public String getProjetoInfo() {
         return "Projeto: Bank | Equipe: Rafael e Lucas";
     }
